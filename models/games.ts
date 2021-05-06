285653662f7bf9e33b3db0029640d22fbd46bce3
Luiizb <56607729+Luiizb@users.noreply.github.com>
2021-05-06 14:52:18 -0300

    Add files via upload

---
+++ models/games.ts
@@ -0,0 +1,25 @@
+import { getModelForClass, prop } from "@typegoose/typegoose";
+
+class Games{
+
+  @prop ({required:[true,"Nick Name é obrigatorio"] })
+  public nickname!: String;
+
+  @prop ({required: [true,"O genero é obrigatorio"]})
+  public genero!:String;
+
+  @prop ()
+  public criadoEm!: Date;
+ 
+  @prop ()
+  public desenvolvedora!: String;
+ 
+ 
+  @prop ()
+  public anoLancamento!: Number;
+
+}
+
+const GamesController = getModelForClass(Games)
+
+//Fiz alteraçao nos modelos do projeto ultilizando  typegosse  para facilitar  o  desenvolvimento e futuras  adequacoes 
